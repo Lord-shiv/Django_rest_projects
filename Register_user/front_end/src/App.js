fdc2006b016193d3d86a6766222a5edb8ad585c8
shivam <shivamchouhan728@gmail.com>
2021-03-21 19:45:50 +0530

    adds class components

--- Register_user/front_end/src/App.js
+++ Register_user/front_end/src/App.js
@@ -2,13 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import Hello from './components/UserForm';
 import UserList from './components/UserList';
+import UserFormClass from './components/UserForms';
 
 function App() {
   return (
     <div className="App">
       <h1>User Form</h1>
       <Hello />
-      <UserList />
+      <UserFormClass />
     </div>
   );
 }
---
+++ Register_user/front_end/src/components/UserForms.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+class UserFormClass extends Component {
+    render() {
+        return (
+            <h1>We are inside class component</h1>
+        )
+    }
+}
+
+export default UserFormClass;
