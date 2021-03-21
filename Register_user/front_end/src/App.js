13b76a48eee420f8e73caf3566309fc4e933b3ae
shivam <shivamchouhan728@gmail.com>
2021-03-21 21:17:15 +0530

    adds events handling

--- Register_user/front_end/src/App.js
+++ Register_user/front_end/src/App.js
@@ -3,11 +3,14 @@ import Hello from './components/UserForm';
 import UserFormClass from './components/UserForms';
 
 function App() {
+  function clicked() {
+    alert("class button clicked.");
+  }
   return (
     <div className="App">
       <h1>User Form</h1>
       <Hello name="shivam" lastname="chouhan" />
-      <UserFormClass email="shivam@chouhan.com" />
+      <UserFormClass email="shivam@chouhan.com" myclick={clicked} />
     </div>
   );
 }
--- Register_user/front_end/src/components/UserForms.js
+++ Register_user/front_end/src/components/UserForms.js
@@ -3,7 +3,10 @@ import React, { Component } from "react";
 class UserFormClass extends Component {
     render() {
         return (
-            <h1>My email is : {this.props.email}</h1>
+            <div>
+                <h1>My email is : {this.props.email}</h1>
+                <button type="submit" onClick={this.props.myclick}>clicked</button>
+            </div>
         )
     }
 }
